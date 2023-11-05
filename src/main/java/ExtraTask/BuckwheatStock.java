45b573d108c65d6b0f28864f253ec4dedd613fce
AnzhelikaGerasimova <ababayan2041@gmail.co>
2023-11-05 19:33:40 +0300

    Not final

--- src/main/java/ExtraTask/BuckwheatStock.java
+++ src/main/java/ExtraTask/BuckwheatStock.java
@@ -3,10 +3,12 @@ package ExtraTask;
 public class BuckwheatStock {
     public static void main(String[] args) {
         int month = 5;
+        int priceBuckwheat = 100;
+        int buckwheatPerMonth = 6;
         int totalFee = 0;
 
-        for (int countMonth = month * 6; countMonth > 0; countMonth -= 6) {
-            totalFee = totalFee + (countMonth * 100);
+        for (int countMonth = month * 1; countMonth > 0; countMonth -= 1) {
+            totalFee = totalFee + countMonth * priceBuckwheat * buckwheatPerMonth;
         }
 
         System.out.println(totalFee);
--- src/main/java/Task1/FizzBuzz.java
+++ src/main/java/Task1/FizzBuzz.java
@@ -2,15 +2,15 @@ package Task1;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        for (int number = 0; number < 100; number++) {
-            if (number % 3 == 0) {
-                System.out.println(number + " fizz");
-            }
-            if (number % 5 == 0) {
-                System.out.println(number + " buzz");
-            }
+        for (int number = 0; number <= 100; number++) {
             if ((number % 3 == 0) && (number % 5 == 0)) {
                 System.out.println(number + " FizzBuzz");
+            }
+            else if (number % 3 == 0) {
+                System.out.println(number + " fizz");
+            }
+            else if (number % 5 == 0) {
+                System.out.println(number + " buzz");
             }
         }
     }
