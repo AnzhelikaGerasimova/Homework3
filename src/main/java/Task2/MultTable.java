b7d2ec2c016efce0ba7132574452d36b0f2b6439
AnzhelikaGerasimova <ababayan2041@gmail.co>
2023-11-06 21:10:23 +0300

    Final

--- src/main/java/Task2/MultTable.java
+++ src/main/java/Task2/MultTable.java
@@ -2,9 +2,9 @@ package Task2;
 
 public class MultTable {
     public static void main(String[] args) {
-        for (int multiplicand1 = 1; multiplicand1 < 10; multiplicand1++) {
-            for (int multiplicand2 = 1; multiplicand2 < 10; multiplicand2++) {
-                System.out.printf("%4d", multiplicand1 * multiplicand2);
+        for (int i = 1; i < 10; i++) {
+            for (int k = 1; k < 10; k++) {
+                System.out.printf("%4d", i * k);
             }
             System.out.println();
         }
